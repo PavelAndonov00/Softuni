737a0fb9ed49fae391ecc5ccbeb4a647c02313d5
Pavel Andonov <god971@abv.bg>
2020-12-20 12:18:08 +0200

    Algo

---
+++ Algorithms/Combinatoric-Problems-Lab/Combinatoric-Problems-Lab/Permutations-Without-Repetitions/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Permutations_Without_Repetitions
+{
+    class Program
+    {
+        static string[] all;
+        static string[] current;
+        static bool[] used;
+        static int[] numbers;
+        static List<int> result;
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            result = new List<int>();
+            numbers = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = i + 1;
+            }
+            GenerateVector(-1);
+
+
+            //all = new string[3] { "A", "B", "b" };
+            //PermOptimized(-1);
+        }
+
+        private static void GenerateVector(int index)
+        {
+            if (index != -1)
+            {
+                result.Add(numbers[index]);
+            }
+
+            if (result.Count == numbers.Length)
+            {
+                Print();
+                return;
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                GenerateVector(i);
+                result.RemoveAt(result.Count - 1);
+            }
+        }
+
+        private static void Print()
+        {
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        private static void PermOptimized(int index)
+        {
+            if (index == all.Length)
+            {
+                Print(all);
+            }
+            else
+            {
+                PermOptimized(index + 1);
+                for (int i = index + 1; i < all.Length; i++)
+                {
+                    Swap(index, i);
+                    PermOptimized(index + 1);
+                    Swap(index, i);
+                }
+            }
+        }
+
+        private static void Swap(int index, int i)
+        {
+            var element = all[index];
+            all[index] = all[i];
+            all[i] = element;
+        }
+
+        private static void Perm(int index)
+        {
+            if (index == all.Length)
+            {
+                Print(current);
+            }
+            else
+            {
+                for (int i = 0; i < all.Length; i++)
+                {
+                    if (!used[i])
+                    {
+                        current[index] = all[i];
+                        used[i] = true;
+                        Perm(index + 1);
+                        used[i] = false;
+                    }
+                }
+            }
+        }
+
+        private static void Print(string[] array)
+        {
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+}
---
+++ Algorithms/Combinatoric-Problems-Lab/Combinatoric-Problems-Lab/Permutations_With_Repetition/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Permutations_With_Repetition
+{
+    class Program
+    {
+        // The main function that recursively prints  
+        // all repeated permutations of the given string.  
+        // It uses data[] to store all permutations one by one 
+        static void allLexicographicRecur(String str, char[] data,
+                                          int last, int index)
+        {
+            int length = str.Length;
+
+            // One by one fix all characters at the given index  
+            // and recur for the subsequent indexes 
+            for (int i = 0; i < length; i++)
+            {
+
+                // Fix the ith character at index and if  
+                // this is not the last index then  
+                // recursively call for higher indexes 
+                data[index] = str[i];
+
+                // If this is the last index then print  
+                // the string stored in data[] 
+                if (index == last)
+                    Console.WriteLine(new String(data));
+                else
+                    allLexicographicRecur(str, data, last,
+                                               index + 1);
+            }
+        }
+
+        // This function sorts input string, allocate memory  
+        // for data(needed for allLexicographicRecur()) and calls 
+        // allLexicographicRecur() for printing all permutations 
+        static void allLexicographic(String str)
+        {
+            int length = str.Length;
+
+            // Create a temp array that will be used by 
+            // allLexicographicRecur() 
+            char[] data = new char[length + 1];
+            char[] temp = str.ToCharArray();
+
+            // Sort the input string so that we get all  
+            // output strings in lexicographically sorted order 
+            Array.Sort(temp);
+            str = new String(temp);
+
+            // Now print all permutaions 
+            allLexicographicRecur(str, data, length - 1, 0);
+        }
+
+        // Driver Code 
+        public static void Main(String[] args)
+        {
+            String str = "abb";
+            Console.Write("All permutations with " +
+                       "repetition of {0} are: \n", str);
+            allLexicographic(str);
+        }
+    }
+}
